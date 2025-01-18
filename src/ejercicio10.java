b53e1429568b8c8af839f3a01f5d0acf8889dee3
Miguel Olarte <mbsic91@gmail.com>
2025-01-17 21:48:41 -0500

    Ejercicio 10 Resuelto

--- src/ejercicio10.java
+++ src/ejercicio10.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class ejercicio10 {
     public static void main(String[] args) {
         /*
@@ -5,5 +7,21 @@ public class ejercicio10 {
          * Crea un programa que calcule la media aritmética de tres números introducidos
          * por el usuario.
          */
+
+         var scanner = new Scanner(System.in);
+
+         var numero1 = 0;
+         var numero2 = 0;
+         var numero3 = 0;
+
+         System.out.println("Ingrese el primer número: ");
+         numero1 = scanner.nextInt();
+         System.out.println("Ingrese el segundo número: ");
+         numero2 = scanner.nextInt();
+         System.out.println("Ingrese el tercer número");
+         numero3 = scanner.nextInt();
+
+         System.out.printf("la media es:  %d", (numero1 + numero2 + numero3) / 3);
+         scanner.close();
     }
 }
